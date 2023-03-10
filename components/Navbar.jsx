01cfc5809e1f516ee586ce9f922c64a9852e2efb
muhsin-web <muhsinabdulrasheed@gmail.com>
2023-03-10 01:22:55 +0100

    updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
 import {AiOutlineShopping} from 'react-icons/ai'
-<<<<<<< HEAD
 import Cart from './Cart'
 import { useStateContext } from '../context/StateContext'
 
 const navbar = () => {
   const {showCart, setShowCart, totalQuantity} = useStateContext()
-=======
 
-const navbar = () => {
->>>>>>> 005cf73b953e0ca79299e90364d7b5ecc052016c
   return (
     <div className="navbar-container">
         <p className="logo">
             <Link href="/">MKD Headphones</Link>
         </p>
 
-<<<<<<< HEAD
         <button type='button' className='cart-icon' onClick={()=> setShowCart(true )}>
             <AiOutlineShopping />
 
@@ -25,13 +20,7 @@ const navbar = () => {
         </button>
 
         {showCart && <Cart/>}
-=======
-        <button type='button' className='cart-icon' onClick="">
-            <AiOutlineShopping />
 
-            <span className="cart-item-qty">1</span>
-        </button>
->>>>>>> 005cf73b953e0ca79299e90364d7b5ecc052016c
     </div>
   )
 }
